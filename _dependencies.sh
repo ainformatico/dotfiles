7a6eddfacb84a7164f15d442e03703d4196f10db
Alejandro Dev. <aeinformatico@gmail.com>
2023-12-30 10:50:58 +0000

    Remove unused files

--- _dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# this script should be executed as sudo/root
-
-# for rbenv, to compile the ruby versions
-apt-get install curl zlib1g-dev libopenssl-ruby openssl libssl-dev libreadline6-dev
